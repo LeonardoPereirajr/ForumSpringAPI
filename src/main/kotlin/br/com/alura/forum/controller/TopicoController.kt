6c12670a40796f6b1d75c7cffcf71e69471102a3
LEONARDO PEREIRA <30580018+LeonardoPereirajr@users.noreply.github.com>
2022-08-07 19:37:02 -0300

    first commit

---
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -0,0 +1,28 @@
+package br.com.alura.forum.controller
+
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoView
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.service.TopicoService
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/topicos")
+class TopicoController(private val service:TopicoService) {
+
+    @GetMapping
+    fun listar(): List<TopicoView> {
+        return service.listar()
+    }
+
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): TopicoView {
+        return service.buscarPorId(id)
+    }
+
+    @PostMapping
+    fun cadastrar(@RequestBody @Valid dto: NovoTopicoForm) {
+        service.cadastrar(dto)
+    }
+}
